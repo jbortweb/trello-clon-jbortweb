6fbf30b548e6e6d2466c3d9b881e2f8103a994e1
jbortweb <jbortweb@gmail.com>
2022-07-18 13:04:11 +0200

    Componente Vista completo

--- src/components/Vista/Vista.jsx
+++ src/components/Vista/Vista.jsx
@@ -1,41 +1,80 @@
 import React from 'react';
-import Header from '../Header/Header';
-import TrelloList from '../TrelloList/TrelloList';
-import { connect } from 'react-redux';
 import './Vista.scss';
+import TrelloList from "../../components/TrelloList/TrelloList.jsx";
+import Header from "../../components/Header/Header.jsx";
+import { connect } from "react-redux";
+import ButtonAction from "../../components/ButtonAction/ButtonAction";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { sort } from "../../services/redux/action";
 
-//Creamos el componente donde se vera la cabecera y la lista con las cartas
+    //Funcion de drop and drag
 
 const Vista = (props) => {
-  const {lists} = props
+    const { lists } = props;
+
+
+    const onDragEnd = (result) => {
+      const { destination, source, draggableId, type } = result;
   
-  return (
-    <div className='panel'>
-        <div>
-            <Header/>
+      if (!destination) {
+        return;
+      }
+      props.dispatch(
+        sort(
+          source.droppableId,
+          destination.droppableId,
+          source.index,
+          destination.index,
+          draggableId,
+          type
+        )
+      );
+    };
+
+    // Generamos la vista de la cabecera y de las listas que vayamos añadiendo
+  
+    return (
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className='App'>
+
+          <div>
+            <Header />
+          </div>
+          <Droppable droppableId="all-list" direction="horizontal" type="list">
+            {(provided) => (
+              <div
+                className="panel"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {React.Children.toArray(lists.map((list, index) => (
+
+                  <TrelloList
+                    listID={list.id}
+                    title={list.title}
+                    cards={list.cards}
+                    key={list.id}
+                    index={index}
+                  />
+                )))}
+                {provided.placeholder}
+                <ButtonAction list />              
+              </div>
+            )}
+          </Droppable>
         </div>
-        <div>
-            {React.Children.toArray(lists.map((list,index) => (
-            <TrelloList
-            listID= {list.id}
-            title= {list.title}
-            cards= {list.cards}
-            key= {list.id}
-            index= {index}
-            />
-            )))}
-        </div>
-    </div>
-  )
-}
+      </DragDropContext>
+    );
+  };
+  
+  const mapStateToProps = state => ({
+    lists: state.lists,
+    cards: state.cards
+  });
 
-// llamamos a las propiedades del estado actualizado
+  const connected = connect(
+    mapStateToProps
+  )(Vista)  
 
-const mapStateToProps = state => ({
-  lists: state.lists
-});
-const connected = connect (
-  mapStateToProps
-)(Vista);
-
-export default connected;
+  export default connected;
+  
